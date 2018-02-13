74a635c579afa8387bfc431ed70290c1bca3424e
Aneta Koppernock <akozuchowska@hotmail.com>
2018-02-14 00:19:22 +0100

    Koniec zadania

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+//zmienne
+var a = prompt('Podaj podstawę trójkąta'),
+	h = prompt('Podaj wysokość trójkąta'),
+	triangleArea = a*h/2;
+
+//wyskakujacy alert z wynikiem
+alert('Pole trójkąta wynosi: ' + triangleArea);	
+//zapis w consoli
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
